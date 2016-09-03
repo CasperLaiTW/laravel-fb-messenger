740d01c6fabbe02709d9d411f0d62ccaee98c7af
Casper Lai <casper@live.cnc.km.edu.tw>
2016-09-03 09:01:03 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Messages/StartButtonTest.php
+++ tests/Messages/StartButtonTest.php
@@ -8,7 +8,6 @@ use Casperlaitw\LaravelFbMessenger\Messages\StartButton;
  */
 class StartButtonTest extends PHPUnit_Framework_TestCase
 {
-
     public function test_to_data()
     {
         $payload = str_random();
