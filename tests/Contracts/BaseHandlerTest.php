f5a27d9e89b38a9cf0036937fcd779918b853e23
Casper Lai <casper.lai@sleepingdesign.com>
2016-09-04 23:35:57 +0800

    test: Add BaseHandlerTest

---
+++ tests/Contracts/BaseHandlerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Casperlaitw\LaravelFbMessenger\Contracts\BaseHandler;
+use Casperlaitw\LaravelFbMessenger\Contracts\Bot;
+use Casperlaitw\LaravelFbMessenger\Messages\Deletable;
+use Casperlaitw\LaravelFbMessenger\Messages\Message;
+use Mockery as m;
+
+/**
+ * User: casperlai
+ * Date: 2016/9/4
+ * Time: 下午11:25
+ */
+class BaseHandlerTest extends TestCase
+{
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    public function test_get_messages()
+    {
+        $expected = [
+            'test',
+            'test2'
+        ];
+        $handler = $this->getMockForAbstractClass(BaseHandler::class);
+        $handler->setMessages($expected);
+
+        $this->assertEquals($expected, $handler->getMessages());
+    }
+
+    public function test_send()
+    {
+        $bot = m::mock(Bot::class)
+            ->shouldReceive('send')
+            ->withAnyArgs()
+            ->andReturnNull()
+            ->getMock();
+        $handler = $this->getMockForAbstractClass(BaseHandler::class);
+        $this->getPrivateProperty(BaseHandler::class, 'bot')->setValue($handler, $bot);
+
+        $message = new MessageStub(null);
+        $message->setDelete(true);
+        $handler->send($message);
+    }
+}
+
+class MessageStub extends Message
+{
+    use Deletable;
+
+    /**
+     * Message to send object
+     * @return \pimax\Messages\Message|array
+     */
+    public function toData()
+    {
+        // TODO: Implement toData() method.
+    }
+}
