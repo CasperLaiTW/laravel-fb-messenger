ce25be4602a8b03e7fd3ab96293a6f0b74fdadc0
Casper Lai <casper.lai@sleepingdesign.com>
2016-11-10 00:51:28 +0800

    feat: Added list template

---
+++ src/Exceptions/DefaultActionInvalidTypeException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: casperlai
+ * Date: 2016/11/9
+ * Time: 上午8:49
+ */
+declare(strict_types = 1);
+
+namespace Casperlaitw\LaravelFbMessenger\Exceptions;
+
+use Exception;
+
+/**
+ * Class DefaultActionInvalidTypeException
+ * @package Casperlaitw\LaravelFbMessenger\Exceptions
+ */
+class DefaultActionInvalidTypeException extends Exception
+{
+
+}
---
+++ src/Exceptions/ListElementCountException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: casperlai
+ * Date: 2016/11/9
+ * Time: 上午10:15
+ */
+declare(strict_types = 1);
+
+namespace Casperlaitw\LaravelFbMessenger\Exceptions;
+
+use Exception;
+
+/**
+ * Class ListElementCountException
+ * @package Casperlaitw\LaravelFbMessenger\Exceptions
+ */
+class ListElementCountException extends Exception
+{
+
+}
