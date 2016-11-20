4f6d87d076b8bcbe9c0d4f9f5bc521b66c4bb1a2
Casper Lai <casper.lai@sleepingdesign.com>
2016-11-20 16:12:31 +0800

    refactor: Pass test

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 $router->get($this->app['config']->get('fb-messenger.custom_url', '/webhook'), 'WebhookController@index');
 $router->post($this->app['config']->get('fb-messenger.custom_url', '/webhook'), 'WebhookController@receive');
-if (config('fb-messenger.debug')) {
+if ($this->app['config']->get('fb-messenger.debug')) {
     $router->get('fb-messenger/debug', 'DebugController@index');
 }
