0d5ca3cab2d19b7af9f77c5d1bc102f1c3d58e8f
Casper Lai <casper.lai@sleepingdesign.com>
2017-01-07 04:31:11 +0800

    test: Add test

--- tests/Contracts/RequestTypeTest.php
+++ tests/Contracts/RequestTypeTest.php
@@ -13,4 +13,22 @@ class RequestTypeTest extends TestCase
 
         $this->assertEquals(Bot::TYPE_POST, $mock->getCurlType());
     }
+
+    public function test_use_delete()
+    {
+        $mock = $this->getMockForTrait(RequestType::class);
+
+        $mock->useDelete();
+
+        $this->assertEquals(Bot::TYPE_DELETE, $mock->getCurlType());
+    }
+
+    public function test_use_get()
+    {
+        $mock = $this->getMockForTrait(RequestType::class);
+
+        $mock->useGet();
+
+        $this->assertEquals(Bot::TYPE_GET, $mock->getCurlType());
+    }
 }
