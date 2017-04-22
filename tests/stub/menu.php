c1d3d6dd615773d5d811f6f32febe92e18602609
Casper Lai <casper.lai@sleepingdesign.com>
2017-04-22 18:32:41 +0000

    Apply fixes from StyleCI

--- tests/stub/menu.php
+++ tests/stub/menu.php
@@ -3,13 +3,13 @@
 use Casperlaitw\LaravelFbMessenger\Messages\UrlButton;
 
 $menu->locale('default', function () use ($menu) {
-   $menu->postback('Test Button', 'TEST_POSTBACK');
-   $menu->webUrl('WebUrl', 'https://github.com/CasperLaiTW/laravel-fb-messenger');
+    $menu->postback('Test Button', 'TEST_POSTBACK');
+    $menu->webUrl('WebUrl', 'https://github.com/CasperLaiTW/laravel-fb-messenger');
 
-   $menu->nested('SubMenu', function () use ($menu) {
-      $menu->postback('SubMenu-Button', 'TEST_SUB_BUTTON');
-      $menu->webUrl(new UrlButton('SubMenu-WebUrl', 'https://github.com/CasperLaiTW/laravel-fb-messenger'));
-   });
+    $menu->nested('SubMenu', function () use ($menu) {
+        $menu->postback('SubMenu-Button', 'TEST_SUB_BUTTON');
+        $menu->webUrl(new UrlButton('SubMenu-WebUrl', 'https://github.com/CasperLaiTW/laravel-fb-messenger'));
+    });
 });
 
 $menu->locale('zh_TW', function () use ($menu) {
