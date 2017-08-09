37fc45a5ee752d359943296dd076c946bd3d4b90
Casper Lai <casper.lai@sleepingdesign.com>
2017-08-09 22:57:29 +0800

    test: Add default action test

--- tests/Messages/ElementTest.php
+++ tests/Messages/ElementTest.php
@@ -1,6 +1,7 @@
 <?php
 use Casperlaitw\LaravelFbMessenger\Collections\ButtonCollection;
 use Casperlaitw\LaravelFbMessenger\Messages\Element;
+use Casperlaitw\LaravelFbMessenger\Messages\UrlButton;
 
 /**
  * User: casperlai
@@ -14,5 +15,18 @@ class ElementTest extends TestCase
     {
         $element = new Element(str_random(), str_random());
         $this->assertInstanceOf(ButtonCollection::class, $element->buttons());
+    }
+
+    public function test_default_action()
+    {
+        $element = new Element(str_random(), str_random());
+        $element->setDefaultAction(new UrlButton('title', 'url'));
+
+        $this->assertArraySubset([
+            'default_action' => [
+                'type' => 'web_url',
+                'url' => 'url',
+            ],
+        ], $element->toData());
     }
 }
