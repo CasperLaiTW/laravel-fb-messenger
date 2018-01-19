6edf6fa79791bcce95b4a3594dc0a216504b7038
Casper Lai <casper.lai@sleepingdesign.com>
2018-01-19 23:09:45 +0800

    feat: Add home url command
    Apply fixes from StyleCI (#90)

--- config/fb-messenger.php
+++ config/fb-messenger.php
@@ -3,11 +3,16 @@ return [
     'debug' => env('APP_DEBUG', false),
     'verify_token' => env('MESSENGER_VERIFY_TOKEN'),
     'app_token' => env('MESSENGER_APP_TOKEN'),
-    'app_secret' => env('MESSENGER_APP_SECRET', null),
+    'app_secret' => env('MESSENGER_APP_SECRET'),
     'auto_typing' => true,
     'handlers' => [
         Casperlaitw\LaravelFbMessenger\Contracts\DefaultHandler::class
     ],
     'custom_url' => '/webhook',
     'postbacks' => [],
+    'home_url' => [
+        'url' => env('MESSENGER_HOME_URL'),
+         'webview_share_button' => 'show',
+         'in_test' => true,
+    ],
 ];
---
+++ src/Commands/HomeUrlCommand.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Casperlaitw\LaravelFbMessenger\Commands;
+
+use Casperlaitw\LaravelFbMessenger\Messages\HomeUrl;
+
+/**
+ * Class HomeUrlCommand
+ *
+ * @package Casperlaitw\LaravelFbMessenger\Commands
+ */
+class HomeUrlCommand extends BaseCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fb:home_url {--d | delete : Delete home url} '.
+    '{--r | read : Read home url}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set home url for chat extensions (default is add)';
+
+    /**
+     * Execute command
+     *
+     * @throws \Casperlaitw\LaravelFbMessenger\Exceptions\NotCreateBotException
+     */
+    public function handle()
+    {
+        if ($this->option('read')) {
+            $this->read();
+            return;
+        }
+
+        $this->addOrRemove();
+    }
+
+    /**
+     * Read domains
+     */
+    private function read()
+    {
+        $command = new HomeUrl();
+        $command->setAction(HomeUrl::TYPE_READ)->useGet();
+        $response = collect(array_get($this->handler->send($command)->getResponse(), 'data.0.home_url', []))
+            ->map(function ($item) {
+                return [$item];
+            });
+
+        $headers = ['Home Url'];
+
+        $this->table($headers, $response);
+    }
+
+    /**
+     *
+     * @throws \Casperlaitw\LaravelFbMessenger\Exceptions\NotCreateBotException
+     */
+    private function addOrRemove()
+    {
+        $command = new HomeUrl(config('fb-messenger.home_url', []));
+
+        if ($this->option('delete')) {
+            $command->setAction(HomeUrl::TYPE_DELETE)->useDelete();
+        }
+
+        $this->comment($this->handler->send($command)->getResponse());
+    }
+}
